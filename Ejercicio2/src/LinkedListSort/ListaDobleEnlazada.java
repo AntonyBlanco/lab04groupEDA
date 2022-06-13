ae83e6fb24fb0e28c07800d3c3e23d2e7dc75467
Edisson Franklin <69181980+Edisson-Acc-UNSA@users.noreply.github.com>
2022-06-12 21:44:06 -0500

    Primeras implementaciones de insersion (Sort) en LinkedLis

---
+++ Ejercicio2/src/LinkedListSort/ListaDobleEnlazada.java
@@ -0,0 +1,40 @@
+package LinkedListSort;
+
+public class ListaDobleEnlazada <T>{
+	Node<T>primero;
+	Node<T>ultimo;
+	
+	public ListaDobleEnlazada() {
+		this.primero= null;
+		this.ultimo=null;
+	}
+	
+	
+	//Agregar Elementos a la lista
+	public void add(T dato ) {
+		Node<T> nuevo = new Node<T>();
+		nuevo.setData(dato);
+		if (this.primero==null) {
+			this.primero= nuevo;
+			//primero.setNextNode(null);
+			primero.setNextNode(null);
+			primero.setPreviousNode(null);
+			this.ultimo=primero;
+		}else {
+			ultimo.setNextNode(nuevo);
+			nuevo.setPreviousNode(ultimo);
+			nuevo.setNextNode(null);
+			ultimo=nuevo;
+		}
+	}
+	
+	//Mostrar elemetos de lista doble
+	public void desplegarLista() {
+		Node <T> actual = new Node<>();
+		actual = primero;
+		while (actual!=null) {
+			System.out.println(actual.getData());
+			actual = actual.getNextNode();
+		}
+	}
+}
---
+++ Ejercicio2/src/LinkedListSort/Test.java
@@ -0,0 +1,17 @@
+package LinkedListSort;
+import com.panayotis.gnuplot.*;
+public class Test {
+	public static void main(String[] args) {
+		ListaDobleEnlazada<Integer> lista =new ListaDobleEnlazada<>();
+		lista.add(17);
+		lista.add(15);
+		lista.add(51);
+		lista.add(54);
+		lista.add(58);
+		lista.add(52);
+		lista.add(7);
+		lista.add(11);
+		
+		lista.desplegarLista();
+	}
+}
